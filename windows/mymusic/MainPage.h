8da79afe300843e55a3f2da186910b261b07faff
Ernesto Vizcaino <vizcaino.erne@gmail.com>
2022-05-21 16:20:22 -0500

    Project created

---
+++ windows/mymusic/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::mymusic::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::mymusic::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
